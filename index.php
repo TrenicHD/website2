f2b0a92bc312e07536946b2fa1e1e26ff29f6fe6
TrenicHD <62098777+TrenicHD@users.noreply.github.com>
2020-03-12 16:31:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>BY TrenicHD</title>
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="footer">
+    <div class="footerContainer">
+        <p class="copyright">©copyright by TrenicHD YT-> TrenicHD Twitter https://twitter.com/Tr3n1cHD </p>
+    </div>
+</footer>
+
+</html>
